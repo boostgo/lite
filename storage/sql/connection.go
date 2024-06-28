1d5933b042831f3750dbe83f6078c82173bbeaf4
lazy_owl <slimsly11@gmail.com>
2024-06-28 17:21:42 +0500

    feat(storage): add default connection settings

--- storage/sql/connection.go
+++ storage/sql/connection.go
@@ -4,6 +4,7 @@ import (
 	"github.com/boostgo/lite/log"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
+	"time"
 )
 
 func Connect(connectionString string, options ...func(connection *sqlx.DB)) (*sqlx.DB, error) {
@@ -11,6 +12,11 @@ func Connect(connectionString string, options ...func(connection *sqlx.DB)) (*sq
 	if err != nil {
 		return nil, err
 	}
+
+	connection.SetMaxOpenConns(10)
+	connection.SetMaxIdleConns(10)
+	connection.SetConnMaxLifetime(time.Second * 10)
+	connection.SetConnMaxIdleTime(time.Second * 10)
 
 	for _, option := range options {
 		option(connection)
