63dde262aa63ac5c639558d9ab0a42d4bcee6cb0
lazy_owl <mm.slimshady@yandex.kz>
2024-07-30 11:08:16 +0500

    feat(kafka) snapshot autocommit mode

--- broker/kafka/snapshot.go
+++ broker/kafka/snapshot.go
@@ -7,7 +7,7 @@ import (
 
 type SnapshotClaim func(message *sarama.ConsumerMessage)
 
-func Snapshot(cfg Config, name string, brokers []string, topic string, snapshotClaim SnapshotClaim) error {
+func Snapshot(cfg Config, name string, brokers []string, topic string, snapshotClaim SnapshotClaim, commit ...bool) error {
 	offsets, err := GetOffsets(brokers, sarama.NewConfig(), topic, sarama.OffsetNewest)
 	if err != nil {
 		return err
@@ -24,6 +24,11 @@ func Snapshot(cfg Config, name string, brokers []string, topic string, snapshotC
 
 	ctx, cancel := context.WithCancel(context.Background())
 
+	var autoCommit bool
+	if len(commit) > 0 {
+		autoCommit = commit[0]
+	}
+
 	consumer.consume(ctx, name, ConsumerGroupHandler(
 		func(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim, message *sarama.ConsumerMessage) {
 			lastOffset, ok := offsets[message.Partition]
@@ -32,6 +37,9 @@ func Snapshot(cfg Config, name string, brokers []string, topic string, snapshotC
 			}
 
 			snapshotClaim(message)
+			if autoCommit {
+				session.MarkMessage(message, "")
+			}
 
 			if lastOffset == message.Offset {
 				delete(offsets, message.Partition)
