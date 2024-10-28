53437e2129b0c95661fd70bf3b9b5a8da0b09e30
yerik.ussayev <yerik.ussayev@tele2.kz>
2024-10-28 16:21:37 +0500

    fix(iterator) upd

--- collections/iterator/iterator.go
+++ collections/iterator/iterator.go
@@ -1,43 +1,68 @@
 package iterator
 
+import (
+	"errors"
+	"github.com/boostgo/lite/types/param"
+	"github.com/boostgo/lite/types/to"
+)
+
 type Iterator[T any] struct {
-	source       []T
-	currentIndex int
+	current int
+	params  []T
+	err     error
 }
 
-func New[T any](source []T) *Iterator[T] {
+func New[T any](params []T) *Iterator[T] {
 	return &Iterator[T]{
-		source: source,
+		params: params,
 	}
 }
 
-func (it *Iterator[T]) Next() bool {
-	return it.currentIndex < len(it.source)
-}
-
-func (it *Iterator[T]) Get() (T, bool) {
-	if it.currentIndex >= len(it.source) {
-		return *new(T), false
-	}
-
-	defer func() {
-		it.currentIndex++
-	}()
-	return it.source[it.currentIndex], true
-}
-
-func (it *Iterator[T]) TryGet() T {
-	value, ok := it.Get()
-	if !ok {
-		return *new(T)
-	}
-
-	return value
+func (it *Iterator[T]) SetError(err error) *Iterator[T] {
+	it.err = err
+	return it
 }
 
 func (it *Iterator[T]) Skip(count int) *Iterator[T] {
 	for i := 0; i < count; i++ {
-		_ = it.TryGet()
+		_, _ = it.Next()
 	}
 	return it
+}
+
+func (it *Iterator[T]) Next() (param.Param, bool) {
+	if it.current >= len(it.params) {
+		return param.Param{}, false
+	}
+
+	defer func() {
+		it.current++
+	}()
+	return param.New(to.String(it.params[it.current])), true
+}
+
+func (it *Iterator[T]) NextString() (string, error) {
+	nextParam, ok := it.Next()
+	if !ok {
+		if it.err != nil {
+			return "", it.err
+		}
+
+		return "", errors.New("wrong params quantity error")
+	}
+
+	return nextParam.String(), nil
+}
+
+func (it *Iterator[T]) NextInt() (int, error) {
+	nextParam, ok := it.Next()
+	if !ok {
+		if it.err != nil {
+			return 0, it.err
+		}
+
+		return 0, errors.New("wrong params quantity error")
+	}
+
+	return nextParam.Int()
 }
