ce2f7ccd795c805e4fa6832f1b1c57628b74c4da
yerik.ussayev <yerik.ussayev@tele2.kz>
2024-11-22 18:37:56 +0500

    feat(sql) connector

--- storage/sql/connection_single.go
+++ storage/sql/connection_single.go
@@ -1,6 +1,7 @@
 package sql
 
 import (
+	"fmt"
 	"github.com/boostgo/lite/log"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
@@ -44,13 +45,72 @@ func MustConnect(connectionString string, options ...func(connection *sqlx.DB)) 
 }
 
 type Connector struct {
-	//
+	host     string
+	port     int
+	username string
+	password string
+	database string
+
+	binaryParameters bool
 }
 
 func NewConnector() *Connector {
 	return &Connector{}
 }
 
+func (connector *Connector) Host(host string) *Connector {
+	connector.host = host
+	return connector
+}
+
+func (connector *Connector) Port(port int) *Connector {
+	connector.port = port
+	return connector
+}
+
+func (connector *Connector) Username(username string) *Connector {
+	connector.username = username
+	return connector
+}
+
+func (connector *Connector) Password(password string) *Connector {
+	connector.password = password
+	return connector
+}
+
+func (connector *Connector) Database(database string) *Connector {
+	connector.database = database
+	return connector
+}
+
+func (connector *Connector) BinaryParameters() *Connector {
+	connector.binaryParameters = true
+	return connector
+}
+
 func (connector *Connector) Build() string {
-	return ""
+	var binaryParameters string
+	if connector.binaryParameters {
+		binaryParameters = " binary_parameters=yes"
+	}
+
+	return fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable%s",
+		connector.host, connector.port,
+		connector.username, connector.password,
+		connector.database,
+		binaryParameters,
+	)
+}
+
+func (connector *Connector) String() string {
+	return connector.Build()
+}
+
+func (connector *Connector) Connect(options ...func(connection *sqlx.DB)) (*sqlx.DB, error) {
+	return Connect(connector.Build(), options...)
+}
+
+func (connector *Connector) MustConnect(options ...func(connection *sqlx.DB)) *sqlx.DB {
+	return MustConnect(connector.Build(), options...)
 }
