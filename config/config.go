b931493e49a09a9e4c0ceb58c663204dc318b0ce
yerik.ussayev <yerik.ussayev@tele2.kz>
2024-11-27 15:20:24 +0500

    doc(config) add comments

--- config/config.go
+++ config/config.go
@@ -6,6 +6,8 @@ import (
 	"os"
 )
 
+// Read export read config file to provided export object.
+// Provided paths can contain as json/yaml file and also .env file
 func Read(export any, path ...string) error {
 	if len(path) == 0 {
 		return cleanenv.ReadEnv(export)
@@ -24,12 +26,14 @@ func Read(export any, path ...string) error {
 	return nil
 }
 
+// MustRead calls Read function and if catch error throws panic
 func MustRead(export any, path ...string) {
 	if err := Read(export, path...); err != nil {
 		panic(err)
 	}
 }
 
+// Get reads OS/environment variable by provided key as param.Param object
 func Get(key string) param.Param {
 	return param.New(os.Getenv(key))
 }
