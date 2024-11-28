0a74ff7d85ea9079f0c512888c9ca876f3d8e07c
yerik.ussayev <yerik.ussayev@tele2.kz>
2024-11-28 15:02:57 +0500

    doc(errs) add comments

--- errs/join.go
+++ errs/join.go
@@ -6,17 +6,32 @@ type joinErrors struct {
 	errors []error
 }
 
+// Join all provided errors into one error
 func Join(errors ...error) error {
+	errsList := make([]error, 0, len(errors))
+	for _, err := range errors {
+		errsList = append(errsList, err)
+	}
 	return &joinErrors{
-		errors: errors,
+		errors: errsList,
 	}
 }
 
+// Unwrap return all errors slice
 func (je joinErrors) Unwrap() []error {
+	if je.errors == nil {
+		je.errors = make([]error, 0)
+	}
+
 	return je.errors
 }
 
+// Error join all errors into one string
 func (je joinErrors) Error() string {
+	if je.errors == nil || len(je.errors) == 0 {
+		return ""
+	}
+
 	message := strings.Builder{}
 	for i := 0; i < len(je.errors); i++ {
 		message.WriteString(je.errors[i].Error())
