1b64fee96dbd0f49112f12a1279823d40a602ca3
yerik.ussayev <yerik.ussayev@tele2.kz>
2024-11-28 15:28:03 +0500

    doc(life) add comments

--- system/life/life.go
+++ system/life/life.go
@@ -15,6 +15,8 @@ var (
 	_life *life
 )
 
+// life controls the application lifetime.
+// Contain global app context, context's cancel function and teardown functions
 type life struct {
 	ctx         context.Context
 	cancel      context.CancelFunc
@@ -22,6 +24,7 @@ type life struct {
 	gracefulLog func()
 }
 
+// Cancel global app context
 func (life *life) Cancel() {
 	life.cancel()
 }
@@ -39,27 +42,34 @@ func instance() *life {
 	return _life
 }
 
+// Init initialize global life instance
 func Init() {
 	instance()
 }
 
+// Context returns global context
 func Context() context.Context {
 	return instance().ctx
 }
 
+// Cancel call global context cancel function
 func Cancel() {
 	instance().cancel()
 }
 
+// GracefulLog set function which calls when Cancel called
 func GracefulLog(gracefulLog func()) {
 	instance().gracefulLog = gracefulLog
 }
 
+// Tear add teardown function which calls after
 func Tear(tear func() error) {
 	l := instance()
 	l.tears = append(l.tears, tear)
 }
 
+// Wait hold current goroutine till global context cancel.
+// If provide wait time it will wait provided time after calling global context cancel
 func Wait(waitTime ...time.Duration) {
 	l := instance()
 
