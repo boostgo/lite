0365b1f7d000236bca8a80ece59466bc6d49f0f5
Yerik Ussayev <yerik.ussayev@tele2.kz>
2024-12-01 17:54:53 +0500

    ref(chunk) make it iterable

--- collections/chunk/chunk.go
+++ collections/chunk/chunk.go
@@ -1,31 +1,48 @@
 package chunk
 
 type Chunk[T ~[]E, E any] struct {
-	source    T
-	chunkSize int
-
-	chunksLength int
+	chunks   []T
+	page     int
+	maxPages int
 }
 
 func New[T ~[]E, E any](source T, chunkSize int) *Chunk[T, E] {
 	return &Chunk[T, E]{
-		source:    source,
-		chunkSize: chunkSize,
-
-		chunksLength: len(source) / chunkSize,
+		chunks:   divide(source, chunkSize, len(source)/chunkSize),
+		page:     0,
+		maxPages: len(source) / chunkSize,
 	}
 }
 
-func (chunk *Chunk[T, E]) Divide() []T {
-	chunks := make([]T, 0, chunk.chunksLength)
+func (chunk *Chunk[T, E]) Get() []T {
+	return chunk.chunks
+}
 
-	for i := 0; i < len(chunk.source); i += chunk.chunkSize {
-		end := i + chunk.chunkSize
-		if end > len(chunk.source) {
-			end = len(chunk.source)
+func (chunk *Chunk[T, E]) Next() bool {
+	return chunk.page < chunk.maxPages
+}
+
+func (chunk *Chunk[T, E]) Read() T {
+	if chunk.page >= chunk.maxPages {
+		return nil
+	}
+
+	defer func() {
+		chunk.page++
+	}()
+	return chunk.chunks[chunk.page]
+}
+
+func divide[T ~[]E, E any](source T, size, length int) []T {
+	chunks := make([]T, 0, length)
+
+	for i := 0; i < len(source); i += size {
+		end := i + size
+		if end > len(source) {
+			end = len(source)
 		}
 
-		chunks = append(chunks, chunk.source[i:end])
+		chunks = append(chunks, source[i:end])
 	}
 
 	return chunks
