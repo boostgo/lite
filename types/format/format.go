02c4de1eb5f9f1e20b5ee275098311c38b199adf
Yerik Ussayev <yerik.ussayev@tele2.kz>
2024-12-02 16:53:17 +0500

    doc(format) add comments

--- types/format/format.go
+++ types/format/format.go
@@ -13,6 +13,12 @@ var (
 	_manySpacesReg = regexp.MustCompile("\\s{2,}")
 )
 
+// Title format input to "title" format.
+// "Title" format is first sentence letter uppercase and other lowercase.
+// Example:
+//
+//	Input: Hello WORLD
+//	Output: Hello world
 func Title(input string) string {
 	if input == "" {
 		return ""
@@ -28,14 +34,25 @@ func Title(input string) string {
 	return clearInput(string(runes))
 }
 
+// Code format input to "code" format.
+// "Code" format is lowercase and no-space text.
+// Example:
+//
+//	Input: Hello-World. 123 !!! 777
+//	Output: hello_world_123_777
 func Code(input string) string {
 	if input == "" {
 		return ""
 	}
 
-	return strings.ReplaceAll(strings.ToLower(clearInput(input)), " ", "_")
+	return strings.ReplaceAll(strings.ToLower(AlphaNumeric(input)), " ", "_")
 }
 
+// Alpha format input to only latin-letter text
+// Example:
+//
+//	Input: Hello-World. 123 !!!
+//	Output: Hello World
 func Alpha(input string) string {
 	if input == "" {
 		return ""
@@ -45,12 +62,19 @@ func Alpha(input string) string {
 	for _, r := range input {
 		if isLatin(r) || r == ' ' {
 			result = append(result, r)
+		} else {
+			result = append(result, ' ')
 		}
 	}
 
 	return clearInput(string(result))
 }
 
+// Numeric format input to only digit text
+// Example:
+//
+//	Input: Hello-World. 123 !!!
+//	Output: 123
 func Numeric(input string) string {
 	if input == "" {
 		return ""
@@ -66,6 +90,11 @@ func Numeric(input string) string {
 	return clearInput(string(result))
 }
 
+// AlphaNumeric format input to text with latin letters & digits (other symbols - erase).
+// Example:
+//
+//	Input: Hello-World. 123 !!!
+//	Output: Hello World 123
 func AlphaNumeric(input string) string {
 	if input == "" {
 		return ""
@@ -73,14 +102,21 @@ func AlphaNumeric(input string) string {
 
 	var result []rune
 	for _, r := range input {
-		if unicode.IsDigit(r) || unicode.IsLetter(r) {
+		if unicode.IsDigit(r) || unicode.IsLetter(r) || r == ' ' {
 			result = append(result, r)
+		} else {
+			result = append(result, ' ')
 		}
 	}
 
 	return clearInput(string(result))
 }
 
+// Cyrillic format input from cyrillic text to latin-code format.
+// Example:
+//
+//	Input: Привет
+//	Output: privet
 func Cyrillic(input string) string {
 	if input == "" {
 		return ""
