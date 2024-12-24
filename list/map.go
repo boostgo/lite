04d83b881ed169e93c611c7846cfe8fcec5f7ba3
yerik.ussayev <yerik.ussayev@tele2.kz>
2024-12-24 12:42:46 +0500

    perf(list) map Keys & Values methods reduce allocation

--- list/map.go
+++ list/map.go
@@ -1,6 +1,10 @@
 package list
 
 func Keys[K comparable, V any](provide map[K]V) []K {
+	if provide == nil || len(provide) == 0 {
+		return []K{}
+	}
+
 	keys := make([]K, 0, len(provide))
 	for key := range provide {
 		keys = append(keys, key)
@@ -9,6 +13,10 @@ func Keys[K comparable, V any](provide map[K]V) []K {
 }
 
 func Values[K comparable, V any](provide map[K]V) []V {
+	if provide == nil || len(provide) == 0 {
+		return []V{}
+	}
+
 	values := make([]V, 0, len(provide))
 	for _, value := range provide {
 		values = append(values, value)
