ee698b9d1b5af461b4e0de5bf0ad1e6650edf4ae
Yerik Ussayev <yerik.ussayev@tele2.kz>
2024-12-26 00:24:28 +0500

    feat(list): add copy map func

--- list/map.go
+++ list/map.go
@@ -23,3 +23,16 @@ func Values[K comparable, V any](provide map[K]V) []V {
 	}
 	return values
 }
+
+func CopyMap[K comparable, V any](provide map[K]V) map[K]V {
+	if provide == nil || len(provide) == 0 {
+		return map[K]V{}
+	}
+	
+	copied := make(map[K]V, len(provide))
+	for key, value := range provide {
+		copied[key] = value
+	}
+
+	return copied
+}
